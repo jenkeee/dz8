f136e89b77409888caa94d16c739c5f4e22631d9
everythink <fsb1skull@gmail.com>
2021-06-02 16:33:31 +0300

    рабочая консоль

--- dz8/dz8_5_console/Program.cs
+++ dz8/dz8_5_console/Program.cs
@@ -1,1 +1,40 @@
-﻿ads
+﻿/*
+ * **Написать программу-преобразователь из CSV в XML-файл с информацией о студентах (6 урок).
+ * 
+ */
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Xml.Linq;
+
+namespace dz8_5_console
+{
+    class Program
+    {
+        /// <summary>Мэджик код</summary>
+        /// <param name="fileNameOpen">Имя csv файла</param>
+        /// <param name="fileNameSave">Имя xml файла</param>
+        static void Converter(string fileNameOpen, string fileNameSave)
+        {
+            string[] lines = File.ReadAllLines(fileNameOpen);
+            string[] headers = { "Name", "Surname", "University", "Faculty", "Department", "Age", "Course", "Group", "City" };
+
+            var xml = new XElement("Students",
+               lines.Where((line, index) => index > 0).Select(line => new XElement("StudentIndo",
+                  line.Split(';').Select((column, index) => new XElement(headers[index], column)))));
+
+            xml.Save(fileNameSave);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа-преобразователь из CSV в XML-файл с информацией о студентах");
+
+            Converter("..\\..\\studentsIN.csv", "..\\..\\studentsOUT.xml");
+
+            Console.WriteLine("Работа программы завершена! \n еще одно решение в копилку ");
+            Console.ReadLine();
+        }
+    }
+}
