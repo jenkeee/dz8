04a1d7084a9fe51eaf9174bd8946889c59db1fa6
everythink <fsb1skull@gmail.com>
2021-06-02 16:09:52 +0300

    добавил еще одно решение 8_4 дз

---
+++ dz8/for8_4/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dz8_4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/// <summary>
+///  нашел непонятное пока для меня  Microsoft.VisualStudio.TestTools.UnitTesting; что это такое?
+/// </summary>
+
+namespace dz8_4.Tests
+{
+    [TestClass()]
+    public class BirthdaysTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Birthdays birthdays = new Birthdays(new List<BirthDay>()
+            {
+                new BirthDay {Surname = "Alex", Name = "Text", Birthday = new DateTime(1980,1,1)},
+            });
+            BirthDay test = new BirthDay { Surname = "Test", Name = "Test", Birthday = new DateTime(1981, 1, 1) };
+            birthdays.Add(test.Surname, test.Name, test.Birthday);
+            Assert.AreEqual(2, birthdays.Count);
+            Assert.AreEqual(test, birthdays[1]);
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Birthdays birthdays = new Birthdays(new List<BirthDay>()
+            {
+                new BirthDay {Surname = "Alex", Name = "Text", Birthday = new DateTime(1980,1,1)},
+            });
+            BirthDay test = new BirthDay { Surname = "Test", Name = "Test", Birthday = new DateTime(1981, 1, 1) };
+            birthdays.Edit(0, test.Surname, test.Name, test.Birthday);
+            Assert.AreEqual(1, birthdays.Count);
+            Assert.AreEqual(test, birthdays[0]);
+            Assert.ThrowsException<ApplicationException>(() => birthdays.Edit(2, test.Surname, test.Name, test.Birthday));
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            BirthDay test = new BirthDay { Surname = "Max", Name = "Faq", Birthday = new DateTime(1980, 1, 1) };
+            Birthdays birthdays = new Birthdays(new List<BirthDay>()
+            {
+                new BirthDay {Surname = "Alex", Name = "Text", Birthday = new DateTime(1980,1,1)},
+                test,
+            });
+            birthdays.Delete(0);
+            Assert.AreEqual(1, birthdays.Count);
+            Assert.AreEqual(test, birthdays[0]);
+            Assert.ThrowsException<ApplicationException>(() => birthdays.Delete(2));
+        }
+
+        [TestMethod()]
+        public void GetStringsTest()
+        {
+            Birthdays birthdays = new Birthdays(new List<BirthDay>()
+            {
+                new BirthDay {Surname = "Alex", Name = "Text", Birthday = new DateTime(1980,1,1)},
+                new BirthDay { Surname = "Max", Name = "Faq", Birthday = new DateTime(1980, 1, 1) },
+                new BirthDay { Surname = "Sax", Name = "Faq", Birthday = new DateTime(1980, 1, 1) },
+            });
+            string[] expected = { "Alex Text 1 января 1980 г.", "Max Faq 1 января 1980 г.", "Sax Faq 1 января 1980 г." };
+            string[] test = birthdays.GetStrings();
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], test[i]);
+            }
+        }
+    }
+}
